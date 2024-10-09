b3ec6860249cecacd02b45f737625cbedc9b8ba3
SIRHVD <huyvd147@gmail.com>
2024-10-09 23:11:28 +0700

    Update TikTokHeaders.h

--- TikTokHeaders.h
+++ TikTokHeaders.h
@@ -352,7 +352,6 @@
 @interface AWEPlayVideoPlayerController : NSObject
 @property(nonatomic) AWEAwemeBaseViewController *container;
 - (void)setPlayerSeekTime:(double)arg1 completion:(id)arg2;
-- (void)setPlaybackRate:(CGFloat)arg1;
 @end
 
 @interface TTKSearchEntranceButton: UIButton
